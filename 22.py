8b5691ad8e267e0fd48990cc558a0fe840558229
AbhiKandS macbook <abhijithksuks@gmail.com>
2024-05-03 00:48:02 +0530

    Add 22.py

---
+++ 22.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+
+print("a.", a)
+print("b. Sliced array: ", a[2:4])
+print("c. Iteration: ")
+for i in a:
+    print(i)
---
+++ 24.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+a1 = np.array([25, 24, -1.5, 101])
+a2 = np.array([23, 51, 15, -63])
+
+print("a. Iteration")
+for i in range(2):
+    b1 = a1 if (i == 0) else a2
+    for j in b1:
+        print(f"{j}, ", end="")
+        b = None
+    print("")
+
+print(f"b. sorted array: \n{np.sort(a1)} \n{np.sort(a2)} \n")
+print("c. concatenated array: ", np.concatenate((a1, a2)))
